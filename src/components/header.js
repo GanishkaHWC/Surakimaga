dd0c014e76cc71f36745f8a8c25694fc46987275
GanishkaHWC <ganishkahwc.19@uom.lk>
2022-07-30 00:15:26 +0530

    2nd commit

---
+++ src/components/header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbarcomp from './Navbarcomp'
+
+const header = () => {
+  return (
+    <div>
+        <Navbarcomp/>
+    </div>
+  )
+}
+
+export default header
